0f7c9e5fc3fc79b2d0ea851d57f65fb2348abeb3
danieloc <daniel.o.c.03@gmail.com>
2017-07-15 19:23:04 +0100

    Remove Span

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -6,7 +6,6 @@ export class Footer extends React.Component {
       <footer>
         <p>
           <span style = {{float: 'left'}}>© 2016 Bubblesortme. All Rights Reserved. </span>
-          <span style = {{float: 'right'}}>D3 Data Visualisation by Mike Bostock</span>
         </p>
       </footer>
     );
