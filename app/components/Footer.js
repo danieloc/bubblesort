10360638ce2dbea5b3d8aa4eaae1416751bb0b17
danieloc <daniel.o.c.03@gmail.com>
2017-07-15 18:53:25 +0100

    Fix Footer Styles

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,7 +4,10 @@ export class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p>© 2016 Bubblesort, Inc. All Rights Reserved.</p>
+        <p>
+          <span style = {{float: 'left'}}>© 2016 Bubblesortme. All Rights Reserved. </span>
+          <span style = {{float: 'right'}}>D3 Data Visualisation by Mike Bostock</span>
+        </p>
       </footer>
     );
   }
